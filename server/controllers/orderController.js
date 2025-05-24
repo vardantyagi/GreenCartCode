f7c768951de0a6196ae1f5e0f88c151519635292
Vardan Tyagi <vardantyagii@gmail.com>
2025-05-24 22:22:45 +0530

    updated userController

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -91,7 +91,7 @@ export const placeOrderStripe = async (req, res) => {
           product_data: {
             name: item.name,
           },
-          unit_amount: Math.floor((item.price + item.price * 0.2)) * 100,
+          unit_amount: Math.floor((item.price + item.price * 0.02)) * 100,
         },
         quantity: item.quantity,
       }
