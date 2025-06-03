388d3dca0477e347e7b02a8e7df4eef4a77ac4f7
Vardan Tyagi <vardantyagii@gmail.com>
2025-06-03 10:07:18 +0530

    bestseller css changed

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -7,7 +7,8 @@ const BestSeller = () => {
   return (
     <div className='mt-16'>
       <p className='text-2xl md:text-3xl font-medium'>Best Seller</p>
-      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 md:gap-6 lg:grid-cols-5 mt-6'>
+      {/* grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 md:gap-6 lg:grid-cols-5 */}
+      <div className='flex flex-wrap mt-6 justify-center sm:justify-start gap-5'> 
         {
           products?.filter((product)=>product?.inStock)?.slice(0,5)?.map((product,i)=>{
             return(
